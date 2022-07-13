97643638e7e6419593c3a8b85c14fa298441b38a
Milica Milakovic <milakovicmilica@yahoo.com>
2022-07-13 15:25:10 +0200

    Recharts Bar Graph and summary

--- lucene-research/movies/src/components/Charts/Recharts.tsx
+++ lucene-research/movies/src/components/Charts/Recharts.tsx
@@ -8,7 +8,37 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  BarChart,
+  Bar,
+  Cell
 } from "recharts";
+
+const barChartData = [
+  {
+    name: "Red",
+    movies: 12,
+  },
+  {
+    name: "Blue",
+    movies: 19,
+  },
+  {
+    name: "Yellow",
+    movies: 3,
+  },
+  {
+    name: "Green",
+    movies: 5,
+  },
+  {
+    name: "Purple",
+    movies: 2,
+  },
+  {
+    name: "Orange",
+    movies: 3,
+  },
+];
 
 const lineChartData = [
   {
@@ -41,6 +71,14 @@ const lineChartData = [
   },
 ];
 
+const colors = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 0.6)",
+  "rgba(255, 206, 86, 0.6)",
+  "rgba(75, 192, 192, 0.6)",
+  "rgba(153, 102, 255, 0.6)",
+  "rgba(255, 159, 64, 0.6)",
+];
 const Recharts = () => {
   return (
     <div className={styles.container}>
@@ -53,7 +91,38 @@ const Recharts = () => {
         />
       </div>
       <div className={styles.charts}>
-        <div className={styles.bar}></div>
+        <div className={styles.bar}>
+          <ResponsiveContainer width="100%" aspect={1}>
+            <BarChart
+              width={500}
+              height={300}
+              data={barChartData}
+              margin={{
+                top: 50,
+                bottom: 50,
+              }}
+            >
+              <CartesianGrid
+                strokeDasharray="1"
+                horizontal
+                vertical
+                stroke="#0D2036"
+              />
+              <XAxis dataKey="name" tick={{ fill: "#fff" }} />
+              <YAxis tick={{ fill: "#fff" }} />
+              <Tooltip cursor={{ fill: '#0D2036', strokeWidth: 2 }}/>
+              <Legend />
+              <Bar dataKey="movies" fill="#8884d8" >
+              {
+          	barChartData.map((entry, index) => {
+            	const color = colors[index];
+            	return <Cell fill={color} />;
+            })
+          }
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
 
         <div className={styles.line}>
           <ResponsiveContainer width="100%" aspect={1}>
@@ -62,13 +131,18 @@ const Recharts = () => {
               height={300}
               data={lineChartData}
               margin={{
-                top: 20,
-                bottom: 20
+                top: 50,
+                bottom: 50,
               }}
             >
-              <CartesianGrid strokeDasharray="1" horizontal vertical stroke="#0D2036" />
-              <XAxis dataKey="name" tick={{ fill: '#fff'}}/>
-              <YAxis tick={{ fill: '#fff'}} />
+              <CartesianGrid
+                strokeDasharray="1"
+                horizontal
+                vertical
+                stroke="#0D2036"
+              />
+              <XAxis dataKey="name" tick={{ fill: "#fff" }} />
+              <YAxis tick={{ fill: "#fff" }} />
               <Tooltip />
               <Legend />
               <Line
@@ -93,9 +167,9 @@ const Recharts = () => {
         <div className={styles.points}>
           <div className={styles.point}>Responive </div>
           <div className={styles.point}>Customizable</div>
-          <div className={styles.point}>Supports animation</div>
           <div className={styles.point}>Offers various types of charts</div>
           <div className={styles.point}>Detailed documentation</div>
+          <div className={styles.point}>More popular </div>
         </div>
       </div>
     </div>
