3aef06989680d6fe238da4573eabde1864a01346
goranagolubovic <45577207+goranagolubovic@users.noreply.github.com>
2022-07-24 22:13:39 +0200

    google chart

--- lucene-research/movies/src/pages/Graphs/Graphs.tsx
+++ lucene-research/movies/src/pages/Graphs/Graphs.tsx
@@ -2,6 +2,7 @@ import styles from "./Graphs.module.css";
 import ChartJS from "../../components/Charts/ChartJS";
 import Recharts from "../../components/Charts/Recharts";
 import Nivo from "../../components/Charts/Nivo";
+import GoogleGraph from "../../components/Charts/GoogleGraph";
 
 const Graphs = () => {
   return (
@@ -13,11 +14,13 @@ const Graphs = () => {
           <a href="https://recharts.org/en-US/" target="_blank" rel="noopener noreferrer"><p>Recharts</p></a>
           <a href="https://www.npmjs.com/package/react-chartjs-2" target='_blank' rel="noopener noreferrer"><p>react-chartjs-2</p></a>
           <a href="https://nivo.rocks/" target="_blank" rel="noopener noreferrer"><p>nivo</p></a>
+          <a href="https://developers.google.com/chart" target="_blank" rel="noopener noreferrer"><p>Google charts</p></a>
         </div>
       </div>
       <Recharts />
       <ChartJS />
       <Nivo />
+      <GoogleGraph/>
     </div>
   );
 };
