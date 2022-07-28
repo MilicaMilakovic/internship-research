27a81b8615d67af6913a20a4f290f88fc3a0a5e1
Milica Milakovic <milakovicmilica@yahoo.com>
2022-07-28 12:34:41 +0200

    Fixed sizes of the Google charts

--- lucene-research/movies/src/components/Charts/GoogleGraph.tsx
+++ lucene-research/movies/src/components/Charts/GoogleGraph.tsx
@@ -1,15 +1,15 @@
-import classNames from 'classnames';
-import React from 'react'
+import classNames from "classnames";
+import React from "react";
 import Chart from "react-google-charts";
 import styles from "./Charts.module.css";
 export const barChartData = [
-  ["Color", "Movies", { role: 'style' }],
-  ["Red", 12, 'color:#ff6384;opacity:1'],
-  ["Blue", 19, 'color:#36a2eb;opacity:0.6'],
-  ["Yellow", 3, 'color:#ffce56;opacity:0.6'],
-  ["Green", 5, 'color:#4bc0c0;opacity:0.6'],
-  ["Purple", 2, 'color:#9966ff;opacity:0.6'],
-  ["Orange", 3, 'color:#ff9f40;opacity:0.6']
+  ["Color", "Movies", { role: "style" }],
+  ["Red", 12, "color:#ff6384;opacity:1"],
+  ["Blue", 19, "color:#36a2eb;opacity:0.6"],
+  ["Yellow", 3, "color:#ffce56;opacity:0.6"],
+  ["Green", 5, "color:#4bc0c0;opacity:0.6"],
+  ["Purple", 2, "color:#9966ff;opacity:0.6"],
+  ["Orange", 3, "color:#ff9f40;opacity:0.6"],
 ];
 const lineChartData = [
   ["Mounth", "Movies"],
@@ -19,37 +19,36 @@ const lineChartData = [
   ["April", 81],
   ["May", 56],
   ["June", 55],
-  ["July", 50]
+  ["July", 50],
 ];
 export const options = {
-
-
-  backgroundColor: 'none',
-  colors: ['#ff6384'],
+  backgroundColor: "none",
+  colors: ["#ff6384"],
   maintainAspectRatio: false,
+  chartArea: {
+    width: "100%",
+    left: 40
+
+  },
+  height: 400,
   legend: {
     textStyle: {
-      color: '#ffffff'
-    }
+      color: "#ffffff",
+    },
   },
   hAxis: {
     textStyle: {
-      color: '#ffffff'
+      color: "#ffffff",
     },
-    gridlines: { color: '#0D2036' }
-
+    gridlines: { color: "#0D2036" },
   },
   vAxis: {
     textStyle: {
-      color: '#ffffff'
+      color: "#ffffff",
     },
 
-    gridlines: { color: '#0D2036' }
-
+    gridlines: { color: "#0D2036" },
   },
-
-
-
 };
 const GoogleGraph = () => {
   return (
@@ -59,7 +58,7 @@ const GoogleGraph = () => {
           className="logo"
           src={`${process.env.PUBLIC_URL}/assets/googlechart.png`}
           alt="logo"
-          height="50"
+          height="40"
         />
         Google Chart
       </div>
@@ -69,7 +68,6 @@ const GoogleGraph = () => {
             chartType="BarChart"
             data={barChartData}
             options={options}
-
           ></Chart>
         </div>
         <div className={classNames(styles.line, styles.googlechart)}>
@@ -91,7 +89,7 @@ const GoogleGraph = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default GoogleGraph
+export default GoogleGraph;
